b75b441f08de8e906df42c5a8bd3a12894d8e929
jahnavi_07 <guvvalajahnavi16@gmail.com>
2025-07-24 16:16:21 +0530

    collections

--- java_fundamentals/src/Abstraction_package_exceptionhandling/Interfaceq1.java
+++ java_fundamentals/src/Abstraction_package_exceptionhandling/Interfaceq1.java
@@ -1,10 +1,50 @@
 package Abstraction_package_exceptionhandling;
+import java.util.Scanner;
+
+interface Playable {
+    void play();
+}
+
+class Veena implements Playable {
+    public void play() {
+        System.out.println("Veena is playing...");
+    }
+}
+
+class Saxophone implements Playable {
+    public void play() {
+        System.out.println("Saxophone is playing...");
+    }
+}
 
 public class Interfaceq1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Playable instrument;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.println("Choose an instrument to play:");
+        System.out.println("1. Veena");
+        System.out.println("2. Saxophone");
+        System.out.print("Enter your choice (1 or 2): ");
+        int choice = scanner.nextInt();
 
-	}
+        if (choice == 1) {
+            Veena veena = new Veena();
+            veena.play(); 
+            instrument = veena;
+        } else if (choice == 2) {
+            Saxophone sax = new Saxophone();
+            sax.play(); 
+            instrument = sax; 
+        } else {
+            System.out.println("Invalid choice.");
+            scanner.close();
+            return;
+        }
 
+        System.out.println("Playing again using Playable reference:");
+        instrument.play();
+
+        scanner.close();
+    }
 }
